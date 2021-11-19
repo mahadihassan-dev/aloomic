4b0d3d4ecc56b250bd78b957a4beb2c13db1a6ea
Mahedi Hassan <mahadihassan.mhd@gmail.com>
2021-11-19 22:02:54 +0600

    Version Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+    // Venobox install for popup
     $('.venobox').venobox();
+    // Client  Slider
+    // $('.client_logoSlider').slick({
+    //     slidesToShow: 4,
+    //     slidesToScroll: 4,
+    //     arrows: false,
+    //     autoplay: false,
+    // }) This is for Client Slider If you want it to active you can Uncomment it
 });
